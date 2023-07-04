52db324a798eec2ead260fdf3e22337e14c8b2ef
unknown <zajda.wojciech@gmail.com>
2023-07-04 14:20:31 +0200

    liblary changed

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-#!/usr/bin/python3
-
-import RPi.GPIO as GPIO
-import time
-
-switch = 26
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(switch, GPIO.IN)
+from gpiozero import Button
+button = Button(2)
 
 while True:
-    time.sleep(0.2)
-    print('Switch status = ', GPIO.input(switch))
-
-GPIO.cleanup()
+    button.wait_for_press()
+    print('You pushed me')
