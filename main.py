924dd604fe013071d5707aaf3976831f8705d844
unknown <zajda.wojciech@gmail.com>
2023-07-04 14:45:25 +0200

    added ui testing ui

--- main.py
+++ main.py
@@ -43,16 +43,16 @@ def main():
     app = QApplication(sys.argv)
     main_win = MyMainWindow()
     main_win.show()
-    
-    button = Button(pin=2, bounce_time=0.2)
-    counter = 0
-    while True:
-        button.wait_for_press()
-        button.wait_for_release()
-        counter = counter+1
-        print('You pushed me')
-        print(counter)
-        label.setText(counter)
+
+    # button = Button(pin=2, bounce_time=0.2)
+    # counter = 0
+    # while True:
+    #     button.wait_for_press()
+    #     button.wait_for_release()
+    #     counter = counter+1
+    #     print('You pushed me')
+    #     print(counter)
+    #     self.label.setText(counter)
 
 
     sys.exit(app.exec_())
