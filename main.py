b3e9b69cd494b174a8286e1c6f5a625d64fe3c4b
unknown <zajda.wojciech@gmail.com>
2023-07-04 14:25:38 +0200

    release check added

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from gpiozero import Button
 button = Button(2)
 
+counter = 0
+
 while True:
     button.wait_for_press()
+    button.wait_for_release()
+    counter = counter+1
     print('You pushed me')
+    print(counter)
