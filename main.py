eceadf400cb8877d3ca3474c5a62560c7407e0fa
unknown <zajda.wojciech@gmail.com>
2023-07-04 14:27:42 +0200

    debouce added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ button = Button(2)
 counter = 0
 
 while True:
-    button.wait_for_press()
+    button.wait_for_press(bounce_time=0.2)
     button.wait_for_release()
     counter = counter+1
     print('You pushed me')
