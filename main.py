4a3849f91f6e5887541c05a4dbc49f7f54655693
unknown <zajda.wojciech@gmail.com>
2023-07-04 14:43:43 +0200

    added ui clasees

--- main.py
+++ main.py
@@ -4,35 +4,60 @@ from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt, QRect
 
-button = Button(pin=2, bounce_time=0.2)
 
-counter = 0
+class MyMainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
         # Get the dimensions of the screen
-screen_resolution = QApplication.instance().desktop().screenGeometry()
-screen_width, screen_height = screen_resolution.width(), screen_resolution.height()
+        screen_resolution = QApplication.instance().desktop().screenGeometry()
+        screen_width, screen_height = screen_resolution.width(), screen_resolution.height()
 
         # Define the label
-label = QLabel()
-label.setText("Huge Label")
-label.setAlignment(Qt.AlignCenter)
+        self.label = QLabel(self)
+        self.label.setText("Huge Label")
+        self.label.setAlignment(Qt.AlignCenter)
 
         # Define the font and size
-font = QFont("Arial", 50, QFont.Bold)
-label.setFont(font)
+        font = QFont("Arial", 50, QFont.Bold)
+        self.label.setFont(font)
 
         # Define the geometry of the label
-label_width = screen_width
-label_height = screen_height
+        label_width = screen_width
+        label_height = screen_height
 
-label.setGeometry(QRect(0, 0, label_width, label_height))
+        self.label.setGeometry(QRect(0, 0, label_width, label_height))
 
         # Set the geometry of the main window
-setGeometry(0, 0, screen_width, screen_height)
-setWindowState(Qt.WindowFullScreen)
-while True:
-    button.wait_for_press()
-    button.wait_for_release()
-    counter = counter+1
-    print('You pushed me')
-    print(counter)
-    label.setText(counter)
+        self.setGeometry(0, 0, screen_width, screen_height)
+        self.setWindowState(Qt.WindowFullScreen)
+
+    def keyPressEvent(self, event):
+        # Allow closing the app by pressing the Escape key
+        if event.key() == Qt.Key_Escape:
+            self.close()
+
+
+def main():
+    app = QApplication(sys.argv)
+    main_win = MyMainWindow()
+    main_win.show()
+    
+    button = Button(pin=2, bounce_time=0.2)
+    counter = 0
+    while True:
+        button.wait_for_press()
+        button.wait_for_release()
+        counter = counter+1
+        print('You pushed me')
+        print(counter)
+        label.setText(counter)
+
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
+
