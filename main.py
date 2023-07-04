6a16539c0993520cfaa0de4761b23e6ee4e349c2
unknown <zajda.wojciech@gmail.com>
2023-07-04 14:29:51 +0200

    debouce added fixed

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from gpiozero import Button
-button = Button(2)
+button = Button(pin=2, bounce_time=0.2)
 
 counter = 0
 
 while True:
-    button.wait_for_press(bounce_time=0.2)
+    button.wait_for_press()
     button.wait_for_release()
     counter = counter+1
     print('You pushed me')
